2e2cc712110f3201f87119aac1f53dd5dfd76eef
Swatantra Singh <68724074+swatantra-coder@users.noreply.github.com>
2022-02-01 10:30:28 +0530

    Update Bubblesort.cpp

--- Sorting Algorithm/Bubblesort.cpp
+++ Sorting Algorithm/Bubblesort.cpp
@@ -21,3 +21,50 @@ int shouldPunish (int roll[])
        }
         return 0;
     }
+
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	int n;
+	printf("Enter the length of array:");
+	scanf("%d",&n);
+	int a[1000];
+	for (int i = 0; i < n; i++)
+	{   printf("Enter the number:");
+		scanf("%d",&a[i]);
+	}
+	printf("Before sorting: ");
+	for(int i=0;i<n;i++)
+    {
+        printf("%d,",a[i]);
+    }
+	
+	printf("\n");
+    for(int i=0;i<n-1;i++)
+    {
+        for(int j=0;j<n-i-1;j++)
+        {
+            if(a[j]>a[j+1])
+            {
+                int temp=a[j];
+                a[j]=a[j+1];
+                a[j+1]=temp;
+                   
+            }
+        }
+    }
+    printf("After sorting: ");
+    for(int i=0;i<n;i++)
+    {
+        printf("%d,",a[i]);
+    }
+	
+
+	return 0;
+		
+
+	
+}
